0e3a72131a1c5af1b0fb19361893325e93fb2c2d
Barikuma <barikumamondelo@gmail.com>
2022-11-16 21:52:41 +0000

    header file update

--- shell.h
+++ shell.h
@@ -44,15 +44,15 @@ typedef struct help_s
 } help_t;
 
 /* --Library Headers-- */
-#include <stdio.h>
-#include <unistd.h>
+#include <signal.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <signal.h>
+#include <stdio.h>
 
 /* --General Functions-- */
 void loop(void);
